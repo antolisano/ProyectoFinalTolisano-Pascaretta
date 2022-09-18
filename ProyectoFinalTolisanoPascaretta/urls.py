39fa71bb98e746af30f80300393b148122e7c26a
Aldana N. Tolisano <tolisanoaldana@gmail.com>
2022-09-18 18:24:39 -0300

    Agregamos los primeros import

--- ProyectoFinalTolisanoPascaretta/urls.py
+++ ProyectoFinalTolisanoPascaretta/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ProyectoFinalTolisanoPascaretta.view import Doctores, Pacientes, Especialidades
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('')
 ]
---
+++ ProyectoFinalTolisanoPascaretta/view.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template import loader
+from AppProyectoFinalTolisanoPascaretta import Doctores, Pacientes, Especialidades
+
+def datosdoctores (self):
+    listadoctores = []
+    
