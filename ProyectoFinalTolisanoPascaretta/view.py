35a1e3927bb2032ff18ae652e4f70c6a99d8e3ae
matpas2191 <matiaspascaretta@hotmail.com>
2022-09-18 19:19:44 -0300

    Prueba4

--- ProyectoFinalTolisanoPascaretta/view.py
+++ ProyectoFinalTolisanoPascaretta/view.py
@@ -4,5 +4,5 @@ from AppProyectoFinalTolisanoPascaretta import Doctores, Pacientes, Especialidad
 
 def datosdoctores (self):
     listadoctores = []
-    nombre1 = nombre1
+    nombre2 = nombre2
     
