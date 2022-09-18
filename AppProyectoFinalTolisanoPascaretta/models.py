3a34906f91b5280d4ad7b89cf29a0341b71052c6
Aldana N. Tolisano <tolisanoaldana@gmail.com>
2022-09-18 18:07:02 -0300

    Creacion de Clases

--- AppProyectoFinalTolisanoPascaretta/models.py
+++ AppProyectoFinalTolisanoPascaretta/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Doctores(models.Model):
+    nombrecompleto = models.CharField(max_length=50)
+    matricula = models.IntegerField()
+    especialidad = models.CharField()
+
+class Pacientes(models.Model):
+    nombrecompleto = models.CharField(max_length=50)
+    dni = models.IntegerField()
+    fechanacimiento = models.DateField()
+
+class Especialidades(models.Model):
+    nombre = models.CharField()
+
